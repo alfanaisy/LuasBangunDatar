e2b83d2e284ded2bffbb443ab373445557e20585
alfanaisy <alfanaisy7@gmail.com>
2021-04-16 10:22:09 +0700

    Add project files.

---
+++ LuasBangunDatar/HitungLuas.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace LuasBangunDatar
+{
+    public abstract class HitungLuas
+    {
+        public abstract double Luas { get; }
+    }
+
+    public class Persegi: HitungLuas
+    {
+        private double Panjang;
+        public Persegi (double Panjang)
+        {
+            this.Panjang = Panjang;
+        }
+        public override double Luas => Panjang * Panjang;
+    }
+
+    public class PersegiPanjang : HitungLuas
+    {
+        private double Panjang, Lebar;
+
+        public PersegiPanjang (double Panjang, double Lebar)
+        {
+            this.Panjang = Panjang;
+            this.Lebar = Lebar;
+        }
+
+        public override double Luas => Panjang * Lebar;
+    }
+
+    public class Segitiga: HitungLuas
+    {
+        public double Alas, Tinggi;
+
+        public Segitiga (double Alas, double Tinggi)
+        {
+            this.Alas = Alas;
+            this.Tinggi = Tinggi;
+        }
+        public override double Luas => 0.5 * Alas * Tinggi;
+    }
+
+    public class Lingkaran: HitungLuas
+    {
+        public double Radius;
+
+        public Lingkaran (double Radius)
+        {
+            this.Radius = Radius;
+        }
+
+        public override double Luas => Math.PI * Radius * Radius;
+    }
+
+    public class LayangLayang: HitungLuas
+    {
+        public double Diagonal1, Diagonal2;
+
+        public LayangLayang (double Diagonal1, double Diagonal2)
+        {
+            this.Diagonal1 = Diagonal1;
+            this.Diagonal2 = Diagonal2;
+        }
+
+        public override double Luas => 0.5 * Diagonal1 * Diagonal2;
+    }
+}
---
+++ LuasBangunDatar/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace LuasBangunDatar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string confirm = "ya";
+            while (confirm == "ya")
+            {
+                Console.WriteLine(
+                    "------ Pilih Bidang Datar ------" +
+                    "\n1. Persegi" +
+                    "\n2. Persegi Panjang" +
+                    "\n3. Segitiga" +
+                    "\n4. Lingkaran" +
+                    "\n5. Layang-Layang"
+                );
+                Console.WriteLine("Pilihan:");
+                int menu = Convert.ToInt32(Console.ReadLine());
+                switch (menu)
+                {
+                    case 1:
+                        Console.WriteLine("Masukkan panjang sisi persegi:");
+                        double panjang = Convert.ToDouble(Console.ReadLine());
+                        Persegi persegi = new Persegi(panjang);
+                        Console.WriteLine("Luas persegi adalah {0}", persegi.Luas);
+                        break;
+                    case 2:
+                        Console.WriteLine("Masukkan panjang sisi persegi panjang:");
+                        panjang = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Masukkan lebar sisi persegi panjang:");
+                        double lebar = Convert.ToDouble(Console.ReadLine());
+                        PersegiPanjang persegiPanjang = new PersegiPanjang(panjang, lebar);
+                        Console.WriteLine("Luas persegi panjang adalah {0}", persegiPanjang.Luas);
+                        break;
+                    case 3:
+                        Console.WriteLine("Masukkan alas segitiga:");
+                        double alas = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Masukkan tinggi segitiga:");
+                        double tinggi = Convert.ToDouble(Console.ReadLine());
+                        Segitiga segitiga = new Segitiga(alas, tinggi);
+                        Console.WriteLine("Luas segitiga adalah {0}", segitiga.Luas);
+                        break;
+                    case 4:
+                        Console.WriteLine("Masukkan jari-jari lingkaran:");
+                        double radius = Convert.ToDouble(Console.ReadLine());
+                        Lingkaran lingkaran = new Lingkaran(radius);
+                        Console.WriteLine("Luas lingkaran adalah {0}", lingkaran.Luas);
+                        break;
+                    case 5:
+                        Console.WriteLine("Masukkan Diagonal 1 Layang-Layang:");
+                        double d1 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Masukkan Diagonal 2 Layang-Layang:");
+                        double d2 = Convert.ToDouble(Console.ReadLine());
+                        LayangLayang layangLayang = new LayangLayang(d1, d2);
+                        Console.WriteLine("Luas layang-layang adalah {0}", layangLayang.Luas);
+                        break;
+                    default:
+                        break;
+                }
+                Console.WriteLine("--------------------------------");
+                Console.WriteLine("Ketik \"Ya\" untuk menghitung luas bidang lain");
+                confirm = Console.ReadLine().ToLower();
+                Console.Clear();
+            }
+        }
+    }
+}
